fd724dc7d0aa3eab2913089c90fd13dcc5b86b33
Cody Schafer <jmesmon@gmail.com>
2012-01-31 00:47:04 -0500

    hmm...

--- lasm.c
+++ lasm.c
@@ -1,6 +1,32 @@
 #include "lasm.h"
 #include "lasm.tab.h"
 
+arg_t  *arg_mk(char *arg)
+{
+	arg_t *x = malloc(sizeof(*x));
+	list_head_init(&x->l);
+	x->arg = arg;
+	return x;
+}
+
+stmt_t *stmt_mk(char *opcode, arg_t *args, attr_t *attrs)
+{
+	stmt_t *x = malloc(sizeof(*x));
+	list_head_init(&x->l);
+	x->opcode = opcode;
+	x->args   = args;
+	x->attrs  = attrs;
+	return x;
+}
+
+attr_t *attr_label_mk(char *label)
+{
+	attr_t *x = malloc(sizeof(*x));
+	list_head_init(&x->l);
+	x->lbl = label;
+	return x;
+}
+
 int main(int argc, char *argv[])
 {
 	yylex();
